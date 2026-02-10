29c77e70b342530ea5a05b9c1f2ba28defbae4d3
Vumirka <rugkevuhvictoria@gmail.com>
2026-02-10 23:18:02 +0200

    Update Predator.java

--- src/domain/Predator.java
+++ src/domain/Predator.java
@@ -1,8 +1,8 @@
 package domain;
 
-public class Predator extends Animal {
-
-    public void hunt() {
-        System.out.println("Predator is hunting...");
+public class Herbivore extends Animal {
+    
+    public void graze() {
+        System.out.println("Herbivore is grazing in the meadow...");
     }
 }
